bf3e68fab8b5f7808806d704dd1961a6f8705d73
Jake Bailey <jacob.b.bailey@gmail.com>
2017-08-24 16:34:08 -0500

    add expiration manager

---
+++ expire/manager.go
@@ -0,0 +1,116 @@
+package expire
+
+import (
+	"sync"
+	"time"
+)
+
+type entry struct {
+	token    *Token
+	onExpire func()
+}
+
+// Manager manages expiration. Periodically, the manager will check its tokens
+// for those that are past their expiration limit. When found, the token's
+// registered onExpire function will be run in a new goroutine (so that the
+// function can call the delete function without deadlocking).
+type Manager struct {
+	checkEvery  time.Duration
+	expireLimit time.Duration
+
+	mu      sync.RWMutex
+	entries map[string]*entry
+	tokens  map[*Token]string
+
+	done chan struct{}
+}
+
+// NewManager creates a new manager that checks for expired entries every
+// checkEvery duration and expires those that are expireLimit or older.
+func NewManager(checkEvery time.Duration, expireLimit time.Duration) *Manager {
+	return &Manager{
+		checkEvery:  checkEvery,
+		expireLimit: expireLimit,
+		entries:     make(map[string]*entry),
+		tokens:      make(map[*Token]string),
+		done:        make(chan struct{}),
+	}
+}
+
+// Run starts the manager.
+func (m *Manager) Run() {
+	go m.run()
+}
+
+// Stop stops the manager.
+func (m *Manager) Stop() {
+	m.done <- struct{}{}
+}
+
+// Acquire acquires a token for the given name. If the specified name already
+// has an entry in the manager, then that existing entry will be expired and
+// its onExpire function run. The new entry replaces it. A Token is returned,
+// which must be updated in order to not be expired.
+//
+// Acquire is safe for concurrent use.
+func (m *Manager) Acquire(name string, onExpire func()) *Token {
+	token := newToken()
+	e := &entry{
+		token:    token,
+		onExpire: onExpire,
+	}
+
+	m.mu.Lock()
+
+	if old := m.entries[name]; old != nil {
+		delete(m.tokens, old.token)
+		go old.onExpire()
+	}
+	m.entries[name] = e
+	m.tokens[token] = name
+
+	m.mu.Unlock()
+
+	return token
+}
+
+// Return retuns a token to the manager, deregistering it. This will not
+// run the associated onExpire function.
+//
+// Return is safe for concurrent use.
+func (m *Manager) Return(token *Token) {
+	m.mu.Lock()
+
+	if name, ok := m.tokens[token]; ok {
+		delete(m.entries, name)
+		delete(m.tokens, token)
+	}
+
+	m.mu.Unlock()
+}
+
+func (m *Manager) run() {
+	ticker := time.NewTicker(m.checkEvery)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-m.done:
+			return
+		case t := <-ticker.C:
+			t = t.Add(-m.expireLimit)
+			m.expire(t)
+		}
+	}
+}
+
+func (m *Manager) expire(threshold time.Time) {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+
+	for _, entry := range m.entries {
+		if !entry.token.after(threshold) {
+			go entry.onExpire()
+		}
+	}
+}
---
+++ expire/token.go
@@ -0,0 +1,31 @@
+package expire
+
+import (
+	"sync/atomic"
+	"time"
+)
+
+// Token keep tracks of the last time it was used. The expiry manager uses
+// checks for tokens that haven't been updated, and expires them.
+type Token struct {
+	name string
+	unix int64
+}
+
+func newToken() *Token {
+	return &Token{
+		unix: time.Now().Unix(),
+	}
+}
+
+// Update updates the internal timestamp of the token. This should be called
+// to keep the token up to date.
+//
+// Update is safe for concurrent use.
+func (t *Token) Update() {
+	atomic.StoreInt64(&t.unix, time.Now().Unix())
+}
+
+func (t *Token) after(u time.Time) bool {
+	return atomic.LoadInt64(&t.unix) > u.Unix()
+}
