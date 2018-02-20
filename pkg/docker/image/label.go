660a55bc8d1d4035018107f136876eb862e65826
Jake Bailey <jacob.b.bailey@gmail.com>
2018-02-19 20:37:25 -0600

    rewrite image label lookup, reduce dependencies

--- pkg/docker/image/label.go
+++ pkg/docker/image/label.go
@@ -1,31 +1,23 @@
 package image
 
 import (
-	"bytes"
 	"context"
-	"fmt"
-	"strings"
 
-	"github.com/docker/cli/cli/command/inspect"
 	"github.com/docker/docker/client"
 )
 
 // GetLabel gets a label from an image.
 func GetLabel(ctx context.Context, cli client.CommonAPIClient, imageID string, labelName string) (string, bool) {
-	var buf bytes.Buffer
-
-	filter := fmt.Sprintf(`{{ index .Config.Labels "%s" }}`, labelName)
-
-	getRefFunc := func(ref string) (interface{}, []byte, error) {
-		return cli.ImageInspectWithRaw(ctx, ref)
-	}
-
-	if err := inspect.Inspect(&buf, []string{imageID}, filter, getRefFunc); err != nil {
+	ii, _, err := cli.ImageInspectWithRaw(ctx, imageID)
+	if err != nil {
 		return "", false
 	}
 
-	value := buf.String()
-	value = strings.TrimSpace(value)
+	if ii.Config == nil {
+		return "", false
+	}
+
+	value := ii.Config.Labels[labelName]
 
 	if value == "" {
 		return "", false
